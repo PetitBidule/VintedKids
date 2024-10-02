9eb93ac12a8e68160a6ac873f7c4a91ac8a10f0c
theo <theo.saint-amand@orange.fr>
2024-10-02 15:10:56 +0200

    feature:bcp-de-choses

--- src/Controller/VintedHomeController.php
+++ src/Controller/VintedHomeController.php
@@ -3,16 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class VintedHomeController extends AbstractController
 {
     #[Route('/', name: 'app_vinted_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $kiwi = $request->query->get('value');
+        // dd($request);
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
         return $this->render('vinted_home/index.html.twig', [
             'controller_name' => 'VintedHomeController',
+            'username' => $user->getEmail(),
+            'id' => $user->getId(),
+            'jeuneKiwi' => $kiwi
         ]);
     }
 }
